010043dd532c96aa15fb49e9436d3882ca36f5c0
maykongpedro <maykonglaffite@gmail.com>
2021-04-10 22:09:37 -0300

    add fundo do gráfico e troca de posição da legenda

--- R/2021-04-09_analises.R
+++ R/2021-04-09_analises.R
@@ -60,6 +60,7 @@ movies_rating %>%
   ggplot2::geom_col(ggplot2::aes(x = year, y = n, fill = binary)) +
   ggplot2::scale_x_continuous(expand = c(0.01,0.01), breaks = movies_rating$year) +
   ggplot2::scale_y_continuous(expand = c(0,0), limits = c(0, 130)) +
+  ggplot2::scale_fill_viridis_d() +
   ggplot2::labs(x = "Ano de lançamento", y = "Quantidade de filmes") +
   ggplot2::theme_minimal() +
   ggplot2::theme(
@@ -75,6 +76,7 @@ movies_rating %>%
   ggplot2::ggplot() +
   ggplot2::geom_col(ggplot2::aes(x = n, y = as.character(year), fill = binary)) +
   ggplot2::scale_x_continuous(expand = c(0,0), limits = c(0, 130)) +
+  ggplot2::scale_fill_viridis_d() +
   ggplot2::labs(y = "Ano de lançamento", x = "Quantidade de filmes", fill = "Teste de Bechdel") +
   ggplot2::theme_minimal() +
   ggplot2::theme(
@@ -106,18 +108,34 @@ movies_rating %>%
     direction = 1 ,
     option = "viridis",
     guide = ggplot2::guide_legend(
-      keyheight = grid::unit(4, units = "mm"),
-      keywidth = grid::unit(8, units = "mm")
+      keyheight = grid::unit(6, units = "mm"),
+      keywidth = grid::unit(9, units = "mm")
     )
   ) +
-  ggplot2::labs(x = "Ano de lançamento", y = "Quantidade de filmes", fill = "Classificação - Bechdel") +
-  ggplot2::theme_minimal() +
-  #ggthemes::theme_clean()+
+  ggplot2::annotate(geom = "text", x = 2005, y = 25, label = "PASS", colour = "black", size = 10) +
+  # ggplot2::annotate(geom = "text", x = 2005, y = 69, label = "FAIL", colour = "black", size = 9) +
+  ggplot2::labs(x = "Ano de lançamento", y = "Quantidade de filmes", fill = "Pontuação - Teste de Bechdel") +
+  ggplot2::theme_minimal(13) +
+  #ggthemes::theme_clean(13)+
   ggplot2::theme(
+    text = ggplot2::element_text(color = "#22211d"),
+    plot.background = ggplot2::element_rect(fill = "#f5f5f2", color = NA),
+    panel.background = ggplot2::element_rect(fill = "#f5f5f2", color = NA),
+    legend.background = ggplot2::element_rect(fill = "transparent", colour = NA),
+    
     axis.line.x = ggplot2::element_line(size = 1),
     axis.text.x = ggplot2::element_text(angle = 90),
-    legend.position = c(.35, .78)
-  )
+    axis.line.y = ggplot2::element_blank(),
+    axis.ticks.y = ggplot2::element_blank(),
+    
+    legend.position = c(.35, .78),
+    legend.title = ggplot2::element_text(face = "bold"),
+    
+    panel.grid.major.y = ggplot2::element_line(size = .7, color = "#dddddd", linetype = "dashed"),
+    #panel.grid = ggplot2::element_blank()
+    )
+
+
 
 # quantidade de filmes por ano e por resultado (%)
 
