9726de1ca57f7859b7b3ebf923bb311c4a45503d
maykongpedro <maykonglaffite@gmail.com>
2021-04-11 14:19:54 -0300

    add formatação do gráfico de percentual

--- R/2021-04-09_analises.R
+++ R/2021-04-09_analises.R
@@ -68,26 +68,9 @@ movies_rating %>%
     axis.text.x = ggplot2::element_text(angle = 90)
   )
 
-# gráfico na horizontal
-movies_rating %>% 
-  dplyr::group_by(year, binary) %>% 
-  dplyr::arrange(year) %>% 
-  dplyr::summarise(n = dplyr::n()) %>%
-  
-  ggplot2::ggplot() +
-  ggplot2::geom_col(ggplot2::aes(x = n, y = as.character(year), fill = binary)) +
-  ggplot2::scale_x_continuous(expand = c(0,0), limits = c(0, 130)) +
-  ggplot2::scale_fill_viridis_d() +
-  ggplot2::labs(y = "Ano de lançamento", x = "Quantidade de filmes", fill = "Teste de Bechdel") +
-  ggplot2::theme_minimal() +
-  ggplot2::theme(
-    axis.line.y = ggplot2::element_line(size = 1)
-  )
-
-
 
 # quantidade de filmes por ano e por resultado da classificação
-font_grafico <- "Lucida Console"
+font_grafico <- "Helvetica"
 windowsFonts(Times=windowsFont("Times New Roman"))
 
 movies_rating %>% 
@@ -165,8 +148,11 @@ movies_rating %>%
 
 
 # quantidade de filmes por ano e por resultado (%)
+windowsFonts(Palatino=windowsFont("Palatino Linotype"))
+font_family <- "Palatino"
 
-movies_rating %>% 
+plot <-
+  movies_rating %>% 
   dplyr::mutate(rating = as.character(rating)) %>% 
   tidyr::unite("rating", c("rating", "descricao"), sep = " - ") %>% 
   
@@ -176,12 +162,62 @@ movies_rating %>%
   ggplot2::ggplot() +
   ggplot2::geom_col(ggplot2::aes(x = year, y = n, fill = rating), position = "fill") +
   ggplot2::scale_x_continuous(expand = c(0.01,0.01), breaks = movies_rating$year) +
-  #ggplot2::scale_y_continuous(expand = c(0,0), limits = c(0, 150), breaks = seq(0, 150, by = 25)) +
-  ggplot2::scale_fill_viridis_d(
-    direction = 1 ,
-    option = "viridis"
-    )
-  
+  ggplot2::scale_y_continuous(expand = c(0,0), labels = scales::percent) +
+  #ggplot2::scale_fill_viridis_d(direction = 1 , option = "viridis") +
+  ggplot2::scale_fill_manual(values = paleta_viridis_modificada) +
+  ggplot2::labs(x = "Ano de lançamento", y = "") +
+  ggplot2::ggtitle("Percentual de filmes por pontuação do teste de Bechdel") +
+  ggplot2::theme_minimal(base_family = font_family) +
+  ggplot2::theme(
+    
+    text = ggplot2::element_text(color = "#22211d"),
+    plot.title = ggplot2::element_text(hjust = 0.5, family = font_family),
+    
+    plot.background = ggplot2::element_rect(fill = "#f5f5f2", color = NA),
+    panel.background = ggplot2::element_rect(fill = "#f5f5f2", color = NA),
+    legend.background = ggplot2::element_rect(fill = "transparent", colour = NA),
+    
+    axis.text.x = ggplot2::element_text(angle = 90),
+    
+    legend.text = ggplot2::element_text(),
+    legend.position = "top",
+    legend.title = ggplot2::element_blank(),
+    
+    plot.margin = ggplot2::unit(c(1, 1, 1, 1), "cm") 
+  ) +
+  ggplot2::geom_label(ggplot2::aes(x = 1990, y = 0.20, label = "PASS"),
+           hjust = 0.4,
+           vjust = 0.5,
+           colour = "#555555",
+           #fill = "white",
+           label.size = NA,
+           family="Palatino",
+           size = 10) +
+  ggplot2::geom_label(ggplot2::aes(x = 1990, y = 0.83, label = "FAIL"),
+                      #hjust = 0.4,
+                      #vjust = 0.0,
+                      colour = "#555555",
+                      fill = "white",
+                      label.size = NA,
+                      family="Palatino",
+                      size = 10)
+
+
+plot
+
+#ggplot2::unit("top",right","botton", "left")
+
+plot %>% 
+ggplot2::ggsave(filename = "./outputs/plot_percent_movies.png",
+                width = 12,
+                height = 8,
+                dpi = 300)
+
+
+# pegando o código das cores da paletta viridis
+paleta_viridis_modificada <- c("#440154", "#443183", "#31688E", "#8FD744")
+
+scales::show_col(viridis::viridis_pal(option = "viridis")(7)) 
 
 
 # quantidade de filmes por gênero
