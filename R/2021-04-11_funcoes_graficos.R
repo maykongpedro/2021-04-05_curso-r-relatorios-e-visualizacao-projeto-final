ae4f9f8b6132962449e0d165c681a2975bb71d5d
maykongpedro <maykonglaffite@gmail.com>
2021-04-12 19:13:24 -0300

    add tema para gráfico de barras

--- R/2021-04-11_funcoes_graficos.R
+++ R/2021-04-11_funcoes_graficos.R
@@ -40,4 +40,44 @@ theme_bechdel_cols <- function(font_family){
   
 } 
 
+
+# tema geral para os gráficos
+theme_bechdel_bars <- function(font_family){
+  ggplot2::theme(
+    
+    # troca os elementos textuais do gráfico
+    text = ggplot2::element_text(color = "#22211d", family = font_family),
+    
+    # deixa o título em negrito
+    plot.title = ggplot2::element_text(face = "bold", hjust = 0.5),
+    
+    # deixa os títulos do eixos em negrito
+    axis.title = ggplot2::element_text(face = "bold"),
+    
+    # coloca uma linha no eixo y
+    axis.line.y = ggplot2::element_line(size = 1),
+    
+    # pinta a parte de plot
+    plot.background = ggplot2::element_rect(fill = "#f5f5f2", color = NA),
+    
+    # pinta o painel
+    panel.background = ggplot2::element_rect(fill = "#f5f5f2", color = NA),
+    
+    # retira o fundo da caixa de legenda
+    legend.background = ggplot2::element_rect(fill = "transparent", colour = NA),
+    
+    # define elementos textuais do título da legenda
+    legend.title = ggplot2::element_text(face = "bold", family = font_family),
+    
+    # define elementos textuais da legenda
+    legend.text = ggplot2::element_text(family = font_family),
+    
+    # posiciona a legenda no canto esquerdo
+    plot.caption = ggplot2::element_text(hjust = 0),
+    
+    # ajusta as margens do gráfico
+    plot.margin = ggplot2::unit(c(1, 1, 1, 1), "cm") 
+    
+  )
   
+} 
